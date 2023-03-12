6cbf7d12f4b1769e8017746cf3d356ad1b6f9f94
ferkano <fjpolanco14@gmail.com>
2023-03-11 23:16:36 -0400

    agregando schema user

---
+++ src/schema/Users.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+const uniqueValidator = require("mongoose-unique-validator");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+  },
+  name: String,
+  lastname: String,
+  passwordHash: String,
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+
+    delete returnedObject._id;
+    delete returnedObject.passwordHash;
+  },
+});
+
+userSchema.plugin(uniqueValidator);
+
+const User = model("User", userSchema);
+
+module.exports = User;
