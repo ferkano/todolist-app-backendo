c0dc47d5a2ee4c2b4978d0a1aedac201a388d27b
ferkano <fjpolanco14@gmail.com>
2023-03-12 19:51:50 -0400

    empezando login

--- src/router/login.routes.js
+++ src/router/login.routes.js
@@ -1,3 +1,20 @@
 const loginRouter = require("express").Router();
+const User = require("../schema/User");
+const bcrypt = require("bcrypt");
+
+loginRouter.post("/", async (req, res, next) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+
+  const passwordCorrect =
+    user === null ? false : bcrypt.compare(password, user.passwordHash);
+
+  if (!(user && passwordCorrect)) {
+    res.status(401).json({
+      error: "invalid user or password",
+    });
+  }
+});
 
 module.exports = loginRouter;
