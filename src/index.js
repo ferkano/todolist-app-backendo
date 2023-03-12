46ec694bb8d71d933fa702cfd6fa361b1872252a
ferkano <fjpolanco14@gmail.com>
2023-03-12 18:23:50 -0400

    se agrego sentry

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 const { mongoose } = require("./database");
 const handleError = require("./middleware/handleError");
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "../.env" });
@@ -16,12 +18,27 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-console.log("hola", process.env.PORT);
+Sentry.init({
+  dsn: "https://a75d25229cdc4b3e9f30ca19bf043b0e@o4504820919304192.ingest.sentry.io/4504827918745600",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 //Controladores
 
 app.use("/api/user", require("./router/user.routes"));
 app.use("/api/login", require("./router/login.routes"));
+
+app.use(Sentry.Handlers.errorHandler());
 
 //ultimos middlewares
 
