57c9189836f8b5b0b3f5d39cdf3bb7fd15bf44e5
ferkano <fjpolanco14@gmail.com>
2023-03-13 22:47:07 -0400

    agregando metodos notes

--- src/router/notes.routes.js
+++ src/router/notes.routes.js
@@ -14,4 +14,38 @@ notesRouter.post("/", (req, res, next) => {
     .catch((err) => next(err));
 });
 
+notesRouter.get("/", (req, res, next) => {
+  Notes.find().then((note) => res.json(note));
+});
+
+notesRouter.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  Notes.findById({ _id: id })
+    .then((notes) => {
+      if (notes) {
+        res.json(notes);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+      next(err);
+    });
+});
+
+notesRouter.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  Notes.findByIdAndDelete({ _id: id }).then(res.end());
+});
+
+notesRouter.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
+
+  Notes.updateOne({ _id: id }, { $set: { title, description } })
+    .then((res) => res.json(res))
+    .catch((err) => res.json(err));
+});
+
 module.exports = notesRouter;
