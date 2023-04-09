40981d24fa2e52d4c9b7428810640757f8862cf7
ferkano <fjpolanco14@gmail.com>
2023-04-09 14:34:23 -0400

    nuevos cambios

--- src/database.js
+++ src/database.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "../.env" });
 
+console.log(process.env.MONGODB_URI);
+
 //conectando con mongodb atlas
 mongoose.set("strictQuery", true);
 mongoose.connect(process.env.MONGODB_URI).then(() => {});
